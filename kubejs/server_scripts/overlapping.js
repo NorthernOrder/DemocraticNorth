fad863887c64836fb81fa08ccfc09f3fe751d373
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-04-22 19:36:17 +0300

    fix wooden support and axe part recipe conflict

---
+++ kubejs/server_scripts/overlapping.js
@@ -0,0 +1,18 @@
+const vanillaPlanks = ["oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "mangrove", "crimson", "warped"];
+
+ServerEvents.recipes(event => {
+	vanillaPlanks.forEach(plank => {
+		const support = `decorative_blocks:${plank}_support`;
+
+		event.remove({ output: support });
+
+		event.recipes.minecraft.crafting_shaped(
+			`3x ${support}`,
+			["PP", "S "],
+			{
+				P: `minecraft:${plank}_planks`,
+				S: "minecraft:stick"
+			}
+		)
+	})
+});
